160cc545c7b5d29f7ec89f559d49b4a72b0220db
Micah Zaayer <micahzaayer@gmail.com>
2019-12-06 15:43:31 -0800

    add scrolling feature.

--- rn-first-app/App.js
+++ rn-first-app/App.js
@@ -2,23 +2,61 @@ import React, { useState } from 'react';
 import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
+  const [enteredGoal, setEnteredGoal] = useState(' ');
+  const [courseGoals, setCourseGoals] = useState([]);
+  const goalInputHandler = (enteredText) => {
+    setEnteredGoal(enteredText);
+  };
+
+  const addGoalHandler = () => {
+    setCourseGoals(currentGoals => [...currentGoals, enteredGoal]);
+  };
   return (
-    <View style={{padding: 50}}>
-      <View style={{flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'}}>
-        <TextInput placeholder="Course Goal" style={{width: '80%', borderBottomColor: 'black', borderBottomWidth: 1, padding: 10 }} 
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
+        <TextInput 
+        placeholder="Course Goal" 
+        style={styles.input} 
+        onChangeText={goalInputHandler} 
+        value={enteredGoal}
         /> 
-        <Button title="ADD" />
+        <Button title="ADD" onPress={addGoalHandler} />
       </View>
-
-      < View />
-
-
+      <FlatList 
+        data={courseGoals} 
+        renderItem={itemData => (
+          < View style={styles.listItem}>
+            <Text>{itemData.item}</Text> 
+          </View>
+        )} 
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  
+  screen: {
+    padding: 50
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  input: {
+    width: '80%',
+    borderColor: 'black',
+    borderWidth: 1,
+    padding: 10
+  },
+  listItem: {
+    padding: 10,
+    marginVertical: 10,
+    backgroundColor: '#ccc',
+    borderColor: 'black',
+    borderWidth: 1
+  }
+
 });
 
 
