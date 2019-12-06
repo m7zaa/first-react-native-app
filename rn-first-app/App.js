0b862f0b0b01bcc552ee36ecf2bca8e7db73dcf1
Micah Zaayer <micahzaayer@gmail.com>
2019-12-06 13:51:13 -0800

    add styling.

--- rn-first-app/App.js
+++ rn-first-app/App.js
@@ -1,21 +1,24 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={{padding: 50}}>
+      <View style={{flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'}}>
+        <TextInput placeholder="Course Goal" style={{width: '80%', borderBottomColor: 'black', borderBottomWidth: 1, padding: 10 }} 
+        /> 
+        <Button title="ADD" />
+      </View>
+
+      < View />
+
+
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  
 });
 
 
