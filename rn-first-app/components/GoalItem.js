7f4d1aaf443b46eb08a3c5716660364511de7cd3
Micah Zaayer <micahzaayer@gmail.com>
2019-12-06 17:21:52 -0800

    add modal to GoalInput.

--- rn-first-app/components/GoalItem.js
+++ rn-first-app/components/GoalItem.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 
 const GoalItem = props => {
-    return (
-      <TouchableOpacity activeOpacity={0.8} onLongPress={props.onDelete.bind(this, props.id)} >
-        <View style={styles.listItem}>
-            <Text>{props.title}</Text> 
-        </View>
-      </TouchableOpacity>
-    );
+  return (
+    <TouchableOpacity activeOpacity={0.8} onLongPress={props.onDelete.bind(this, props.id)} >
+      <View style={styles.listItem}>
+        <Text>{props.title}</Text>
+      </View>
+    </TouchableOpacity>
+  );
 };
 
 const styles = StyleSheet.create({
@@ -18,6 +18,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#ccc',
     borderColor: 'black',
     borderWidth: 1
-    }
+  }
 })
 export default GoalItem;
